991a1f3ff7fc00038d50d3ce50a203a086e442cc
Archeterr <archeter@hotmail.com>
2023-12-14 00:05:55 +0300

    first commit

---
+++ fightgame/android/app/src/main/kotlin/com/example/fightgame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fightgame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ fightgame/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:fightgame/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/login.jpg"),
+                  fit: BoxFit.none,
+                  scale: 0.5,
+                  alignment: Alignment.bottomCenter),
+            ),
+            child: const Login()),
+      ),
+    );
+  }
+}
